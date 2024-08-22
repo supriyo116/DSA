7887ad8d84fd1c4da36bb2533871e451dd55c109
Supriyo <supriyo2003@gmail.com>
2024-08-22 22:47:46 +0530

    NA

--- sorting.cpp
+++ sorting.cpp
@@ -14,6 +14,29 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 
 int main() {   
 
